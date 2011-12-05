c70813dbda864bebff19bf9b920959276191dfd9
saubcy <saubcy@gmail.com>
2011-12-05 22:07:41 +0800

    添加缺失的库

--- gen/R.java.d
+++
@@ -1,5 +0,0 @@
-C:\Documents and Settings\Administrator\git\Android-Pipeline\gen\com\saubcy\lab\pipeline\R.java \
- : C:\Documents and Settings\Administrator\git\Android-Pipeline\Resource\MiidiGold\drawable\icon.png \
-C:\Documents and Settings\Administrator\git\Android-Pipeline\Resource\MiidiGold\layout\main.xml \
-C:\Documents and Settings\Administrator\git\Android-Pipeline\Resource\MiidiGold\values\strings.xml \
-C:\Documents and Settings\Administrator\git\Android-Pipeline\AndroidManifest.xml \
